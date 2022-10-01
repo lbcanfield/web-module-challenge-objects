a455cb03714471ba73114b68db016d9c1e5a5bc3
lbcanfield <lewie.b.canfield@gmail.com>
2022-10-01 14:28:17 -0500

    Task 7 is complete

--- index.js
+++ index.js
@@ -203,11 +203,13 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(tArray) 
+{
+  let lri = tArray.length - 1;
+  return tArray[lri].name + " gave the restaurant a " + tArray[lri].rating + " star review, and their feedback was: " + tArray[lri].feedback;
 } 
 
-
+console.log( getLastReview(reviews) );
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
